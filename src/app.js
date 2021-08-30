f803d87e506e1640c3dfcc5e65e55d06c8ea6aa1
tesigmond <tesigmond@gmail.com>
2021-08-29 20:37:44 -0700

    Showing windspeed

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#weather-description");
   let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "eaf7d01f74cab187ef4b58c7a2bb6662";
